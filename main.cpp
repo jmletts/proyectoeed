08119149f5374350ac7632807f70306f8de88fdf
joaquin <joaquir520@live.com>
2024-10-07 20:56:12 -0500

    ok

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
-≈#include <iostream>
+#include <iostream>
 #include <iomanip>
 
 using namespace std;
+
+struct estudiante
+{
+    string nombre;
+    
+};
 
 int main() {
     cout << "Hola, mundo!" << endl;
     return 0;
 }
+
