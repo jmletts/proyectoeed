d4d07380d53abcd5316f1b3948d3c0b444b4e3e2
jmletts <153383513+jmletts@users.noreply.github.com>
2024-11-06 16:57:22 -0500

    Actualizar listadoble.cpp

--- bibliotecas/listadoble.cpp
+++ bibliotecas/listadoble.cpp
@@ -2,85 +2,149 @@
 
 using namespace std;
 
-struct nodo
-{
+struct nodo {
     int dato;
-    nodo *sig; 
-    nodo *ant;  
+    nodo *sig;
+    nodo *ant;
 };
 
-
-
-void insertarElementos();
+void insertarElementosI(nodo *&inicio, int n);
 void insertarElementosF(nodo *&inicio, int n);
 void mostrarI(nodo *inicio);
 void mostrarF(nodo *inicio);
+void eliminarElemento(nodo *&inicio, int n);
 
-
-int main(int argc, char const *argv[])
-{
+int main() {
     nodo *inicio = NULL;
-    nodo *fin = NULL;
-    
+    int opcion, n;
+
+    do {
+        cout << "\n--- Menú de Opciones ---\n";
+        cout << "1. Insertar al inicio\n";
+        cout << "2. Insertar al final\n";
+        cout << "3. Mostrar desde el inicio\n";
+        cout << "4. Mostrar desde el final\n";
+        cout << "5. Eliminar un elemento\n";
+        cout << "6. Salir\n";
+        cout << "Seleccione una opción: ";
+        cin >> opcion;
+
+        switch (opcion) {
+            case 1:
+                cout << "Ingrese un número para insertar al inicio: ";
+                cin >> n;
+                insertarElementosI(inicio, n);
+                break;
+            case 2:
+                cout << "Ingrese un número para insertar al final: ";
+                cin >> n;
+                insertarElementosF(inicio, n);
+                break;
+            case 3:
+                cout << "Mostrando lista desde el inicio:\n";
+                mostrarI(inicio);
+                break;
+            case 4:
+                cout << "Mostrando lista desde el final:\n";
+                mostrarF(inicio);
+                break;
+            case 5:
+                cout << "Ingrese el elemento que desea eliminar: ";
+                cin >> n;
+                eliminarElemento(inicio, n);
+                break;
+            case 6:
+                cout << "Saliendo...\n";
+                break;
+            default:
+                cout << "Opción no válida.\n";
+                break;
+        }
+    } while (opcion != 6);
+
     return 0;
 }
 
-void insertarElementosI(nodo *&inicio, int n)
-{
-    nodo *nuevo = new nodo(); // s reserva mmemoria
-    nuevo->dato = n; 
-    nuevo->sig = inicio; // el puntero seigueinete apuntara al inicio
-    nuevo->ant = NULL; // como sera el primero en la lista entoces su anteriior sera null
+void insertarElementosI(nodo *&inicio, int n) {
+    nodo *nuevo = new nodo();
+    nuevo->dato = n;
+    nuevo->sig = inicio;
+    nuevo->ant = NULL;
 
-    if (inicio != NULL)
-    {
-        inicio->ant = nuevo; // entonces el anterior de inicio se actualiza como nuevo
+    if (inicio != NULL) {
+        inicio->ant = nuevo;
     }
-    
-    inicio = nuevo; // se actualiza para que sea el primero en la lista
 
+    inicio = nuevo;
 }
 
-void insertarElementosF(nodo *&inicio, int n)
-{
+void insertarElementosF(nodo *&inicio, int n) {
     nodo *nuevo = new nodo();
     nodo *aux = inicio;
-    nuevo->dato = n; 
-    nuevo->sig = NULL; // el nuevo sieguiente sera null porque este sera el ultimo en a la lista
+    nuevo->dato = n;
+    nuevo->sig = NULL;
 
-    if (inicio == NULL) // verificar el inicio de la lista
-    {
-        nuevo->ant = NULL; // el anterior debera ser nuloya queno tiene un nodo anterior
-        inicio = nuevo; // y se actaulize para que inicio apunte a nuevo
+    if (inicio == NULL) {
+        nuevo->ant = NULL;
+        inicio = nuevo;
         return;
-
     }
-    
-    while (aux->sig != NULL) // recorremos desde el inicio hasta encontrar uno que el siegiinte sa null
-    {
-        aux = aux->sig; 
-    }
-    
-    aux->sig = nuevo; // aasigna el ulimo nodo con el nuevo
-    nuevo->ant = aux; // el puntero anterior del nuevo, apunta al antiguo nodo de la lista
 
+    while (aux->sig != NULL) {
+        aux = aux->sig;
+    }
+
+    aux->sig = nuevo;
+    nuevo->ant = aux;
 }
 
-void mostrarI(nodo *inicio)
-{
+void mostrarI(nodo *inicio) {
     nodo *aux = inicio;
 
-    while (aux != NULL)
-    {
+    while (aux != NULL) {
         cout << aux->dato << " ";
         aux = aux->sig;
     }
     cout << endl;
-
 }
 
-void mostrarF(nodo *inicio)
-{
+void mostrarF(nodo *inicio) {
+    if (inicio == NULL) return;
+
     nodo *aux = inicio;
-    
+    while (aux->sig != NULL) {
+        aux = aux->sig;
+    }
+
+    while (aux != NULL) {
+        cout << aux->dato << " ";
+        aux = aux->ant;
+    }
+    cout << endl;
+}
+
+void eliminarElemento(nodo *&inicio, int n) {
+    nodo *aux = inicio;
+
+    while (aux != NULL && aux->dato != n) {
+        aux = aux->sig;
+    }
+
+    if (aux == NULL) {
+        cout << "Elemento no encontrado.\n";
+        return;
+    }
+
+    if (aux->ant != NULL) {
+        aux->ant->sig = aux->sig;
+    } else {
+        inicio = aux->sig;
+    }
+
+    if (aux->sig != NULL) {
+        aux->sig->ant = aux->ant;
+    }
+
+    delete aux;
+    cout << "Elemento eliminado.\n";
 }
